2e7f78d2577e924bf7b392de2d0a4a65da5c5b5f
Nadeen Saleh <nsaleh2013@gmail.com>
2016-11-26 12:24:22 -0500

    gather all new game variables

--- app/src/main/java/com/cop4331/oneshot/NewGameActivity.java
+++ app/src/main/java/com/cop4331/oneshot/NewGameActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import com.cop4331.networking.AccountManager;
 public class NewGameActivity extends Activity {
 
     private EditText mPrompt    = null;
+    private Spinner mSpinner = null;
 
     @Nullable
     @Override
@@ -25,25 +27,29 @@ public class NewGameActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newgame_layout);
 
-        Spinner spinner = (Spinner) findViewById(R.id.gameDurationSpinner);
+        mSpinner = (Spinner) findViewById(R.id.gameDurationSpinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.gameduration_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        mSpinner.setAdapter(adapter);
 
-        ((Button)findViewById(R.id.createGameButton)).setOnClickListener(mSignupListener);
+        (findViewById(R.id.createGameButton)).setOnClickListener(mCreateGameListener);
 
         mPrompt = ((EditText)findViewById(R.id.promptText));
     }
 
-    private final Button.OnClickListener mSignupListener = new Button.OnClickListener() {
+    private final Button.OnClickListener mCreateGameListener = new Button.OnClickListener() {
         @Override
         public void onClick(View view) {
-            AccountManager manager = AccountManager.getInstance();
+            //AccountManager manager = AccountManager.getInstance();
             String prompt   = mPrompt.getText().toString();
+            String duration = mSpinner.getSelectedItem().toString();
+            Log.d("NEW GAME", prompt);
+            Log.d("NEW GAME", duration);
+
         }
     };
 
